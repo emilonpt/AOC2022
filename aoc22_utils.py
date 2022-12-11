f5a9b3e23f5dfccdc8f447cff8bfec01d180054f
Manuel Pinheiro <manuelpinheiro3@gmail.com>
2022-12-11 13:58:35 +0000

    add another util

--- aoc22_utils.py
+++ aoc22_utils.py
@@ -5,3 +5,15 @@ def read_input_file_without_trailing_newlines(day:int) -> list[str]:
             if l[i][-1] == '\n':
                 l[i] = l[i][:-1]
     return l
+
+def split_input_list_on_empty_string(input_list):
+    output_list = []
+    temp_list = []
+    for i in input_list:
+        if i == '':
+            output_list.append(temp_list)
+            temp_list = []
+        else:
+            temp_list.append(i)
+    output_list.append(temp_list)
+    return output_list
